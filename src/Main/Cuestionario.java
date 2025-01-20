e9322fde3223418a3c801d5626f0860bf1c4963b
Marco560MX <marcodeleal89@gmail.com>
2025-01-20 04:28:03 -0600

    Actualizado 100% clase cuestionario

--- src/Main/Cuestionario.java
+++ src/Main/Cuestionario.java
@@ -1,7 +1,6 @@
 package Main;
 
 import javax.swing.*;
-import java.util.Scanner;
 
 public class Cuestionario {
     public static void main(String[] args) {
@@ -9,10 +8,9 @@ public class Cuestionario {
         String dato2;
         int userSelect;
         InicioSesion inicio = new InicioSesion();
-        Scanner ingreso = new Scanner(System.in);
 
         JOptionPane.showMessageDialog(null, "!Bienvenid@ al cuestionario");
-        JOptionPane.showMessageDialog(null,"Version: Alpha 6.0.14");
+        JOptionPane.showMessageDialog(null,"Version: Alpha 6.0.15");
         JOptionPane.showMessageDialog(null, "!Para continuar es necesario iniciar sesion!");
         JOptionPane.showMessageDialog(null, "¿Iniciar sesion con?");
         userSelect = Integer.parseInt(JOptionPane.showInputDialog("(1) Usuario (2) Administrador"));
@@ -24,10 +22,8 @@ public class Cuestionario {
                 break;
 
             case 2:
-                System.out.println("\nIngrese su usuario");
-                dato1 = ingreso.next();
-                System.out.println("\nIngrese su contraceña");
-                dato2 = ingreso.next();
+                dato1 = JOptionPane.showInputDialog("Ingrese su usuario");
+                dato2 = JOptionPane.showInputDialog("Ingrese su contraseña");
                 inicio.setAdministrador(dato1, dato2);
                 break;
 
